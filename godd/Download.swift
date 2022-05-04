e32559d5bc3b513fb435f9c2f8ab03b6d1775944
ArsalanBudozhapov <04433973+ArsalanBudozhapov@users.noreply.github.com>
2022-05-04 19:34:33 +0400

    sdvdsvs

--- godd/Download.swift
+++ godd/Download.swift
@@ -5,16 +5,17 @@
 //  Created by мак on 03.05.2022.
 //
 
-//import Foundation
-//
-//class Download {
-//  var isDownloading = false
-//  var progress: Float = 0
-//  var resumeData: Data?
-//  var task: URLSessionDownloadTask?
-//  var track: Track
-//
-//  init(track: Track) {
-//    self.track = track
-//  }
-//}
+import Foundation
+
+class Download {
+  
+  var isDownloading = false
+  var progress: Float = 0
+  var resumeData: Data?
+  var task: URLSessionDownloadTask?
+  var movie: Movie
+  
+  init(movie: Movie) {
+    self.movie = movie
+  }
+}
